fdac729e231d3295d8534f8e7bfb4fc52f4bc0cc
taufiksatrian <72427297+taufiksatrian@users.noreply.github.com>
2022-12-06 18:12:24 +0700

    Responsi PRaktikum Mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    canLoad: [AutoLoginGuard]
+  },
+  {
+    path: 'obat',
+    loadChildren: () => import('./obat/obat.module').then( m => m.ObatPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'obat-tambah',
+    loadChildren: () => import('./obat-tambah/obat-tambah.module').then( m => m.ObatTambahPageModule)
+  },
+  {
+    path: 'obat-edit/:id',
+    loadChildren: () => import('./obat-edit/obat-edit.module').then( m => m.ObatEditPageModule)
   },
 ];
 
